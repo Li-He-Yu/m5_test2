a4efb58ce9e98f2dfc41921b0128360e76a4a0f7
李和豫 <71595032+Li-He-Yu@users.noreply.github.com>
2025-12-12 22:59:35 +0800

    [refactor] Split html into 3 file; change names;
    - test stable
    - no any change on logic
    - rename "flowview.html" --> "flowchart_panel_template.html"
    - Split the whole template into isolated 3 file:
    	html: "flowchart_panel_template.html",
    	css : "flowchart_panel.css",
    	js 	: "flowchart_panel.js";
    - add some comment into file

--- src/extension.ts
+++ src/extension.ts
@@ -419,6 +419,11 @@ function parseNodeMeta(metaStr: string): NodeMeta {
     catch (e) { console.error('Error parsing node meta:', e); return {}; }
 }
 
+/**
+ - What: a tiny helper that generates a random string (the “nonce”).
+ - Why: Webview uses a Content Security Policy (CSP) that blocks inline scripts unless they carry a matching nonce.
+ - Note: ONLY scripts who will be EXEcuted need to carry nonce
+ */
 function getNonce(): string {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let nonce = '';
@@ -435,23 +440,39 @@ async function getWebviewHtmlExternal(
     nodeOrder: string[],
     pseudocode: string = ''
 ): Promise<string> {
-    const templateUri = vscode.Uri.joinPath(context.extensionUri, 'media', 'flowview.html');
+    // 1. get the Html templates
+    const templateUri = vscode.Uri.joinPath(context.extensionUri, 'media', 'flowchart_panel_template.html');
     const bytes = await vscode.workspace.fs.readFile(templateUri);
     let html = new TextDecoder('utf-8').decode(bytes);
 
+    // 2. get correct path
+    // JavaScripts Mermaid module
     const mermaidUri = webview.asWebviewUri(
         vscode.Uri.joinPath(context.extensionUri, 'media', 'mermaid.min.js')
     );
-    console.log('Mermaid URI:', mermaidUri.toString());
+    // the css file
+    const cssUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "media", "flowchart_panel.css")
+    );
+    // the JS file
+    const jsUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "media", "flowchart_panel.js")
+    );
+
+    // 3. generate nonce
     const nonce = getNonce();
 
+    // 4. replace placeholder
     html = html
         .replace(/%%CSP_SOURCE%%/g, webview.cspSource)
         .replace(/%%NONCE%%/g, nonce)
         .replace(/%%MERMAID_JS_URI%%/g, mermaidUri.toString())
         .replace(/%%MERMAID_CODE%%/g, mermaidCode)
         .replace(/%%NODE_ORDER_JSON%%/g, JSON.stringify(nodeOrder))
-        .replace(/%%PSEUDOCODE%%/g, escapeHtml(pseudocode)); 
+        .replace(/%%PSEUDOCODE%%/g, escapeHtml(pseudocode))
+        .replace(/%%FLOWCHART_PANEL_CSS_URI%%/g, cssUri.toString())
+        .replace(/%%FLOWCHART_PANEL_JS_URI%%/g, jsUri.toString())
+        ; 
 
     return html;
 }
